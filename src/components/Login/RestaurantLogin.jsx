929863463c1d4950af7733e4b1746c6c3331ff52
tavneetkhurana <tavneetsingh2000@gmail.com>
2022-10-11 12:31:49 +0530

    login validations

--- src/components/Login/RestaurantLogin.jsx
+++ src/components/Login/RestaurantLogin.jsx
@@ -1,6 +1,7 @@
 import React , { useRef } from 'react'
 import './Login.css'
 import axios from 'axios'
+import { useState } from 'react'
 import { images } from '../../constants'
 import { useSelector, useDispatch } from 'react-redux'
 import { LoginStart, LoginSuccess } from '../../actions/index'
@@ -15,6 +16,9 @@ const RestaurantLogin = () => {
   
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+
     dispatch(LoginStart());
     try{
       const res = await axios.post('http://localhost:1234/restaurant/login', {
@@ -22,15 +26,38 @@ const RestaurantLogin = () => {
         password: password.current.value
       })
       dispatch(LoginSuccess({payload: res.data}));
-      window.location.replace("/restaurant/addmenu")  
+      window.location.replace("/foods")  
     }catch(err){
-      console.log("Invalid Credentials!");
+       alert("Invalid Credentials!");
     }
   }
 
-  console.log(restaurant)
+  // console.log(restaurant)
 
+  
+  const initalValues = { email: "" , password: ""};
+  const [formValues, setFormValues] = useState(initalValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]:value});
+    console.log(formValues);
+  }
+
+  const validate = (values) => {
+    const errors = {}
+    const regex =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!values.email) {
+      errors.email = "Email is required";
+    }
+    if(!values.password) {
+      errors.password = "Password is required";
+    }
+    return errors;
+  }
 
   return (
     <>
@@ -40,9 +67,13 @@ const RestaurantLogin = () => {
                 <form action="" className='login-formControl' onSubmit={handleSubmit}>
                     <div className='login-inputFields'>
                       <label htmlFor="">Email ID</label>
-                      <input type="email"  ref={email}/>
+                      <input type="email"  ref={email} value={formValues.email} 
+                        onChange={handleChange} name='email'/>
+                         <span>{formErrors.email}</span>
                       <label htmlFor="">Password</label>
-                      <input type="password"  ref={password}/>
+                      <input type="password"  ref={password} value={formValues.password} 
+                        onChange={handleChange} name='password'/>
+                         <span>{formErrors.password}</span>
                     </div>
                     <input value='Login' type='submit' href='foods'  />
                 </form>
