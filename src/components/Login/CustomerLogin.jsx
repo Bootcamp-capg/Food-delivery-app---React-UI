b179e05e8f52098e16ed8c4764ff601dd307a763
tavneetkhurana <tavneetsingh2000@gmail.com>
2022-10-11 13:24:47 +0530

    Registration Validations

--- src/components/Login/CustomerLogin.jsx
+++ src/components/Login/CustomerLogin.jsx
@@ -51,9 +51,16 @@ const CustomerLogin = () => {
 
     if(!values.email) {
       errors.email = "Email is required";
+    } else if(!regex.test(values.email)) {
+      errors.email = "This is not a valid email format";
     }
+    
     if(!values.password) {
       errors.password = "Password is required";
+    } else if(values.password < 4 ) {
+      errors.password = "Password must be more than 4 characters";
+    } else if(values.password > 10 ) {
+      errors.password = "Password cannot exceed more than 10 characters";
     }
     return errors;
   }
--- src/components/Register/CustomerRegister.jsx
+++ src/components/Register/CustomerRegister.jsx
@@ -5,35 +5,97 @@ import axios from 'axios'
 
 const Customer = () => {
 
-  const [name, setName] = useState("");
-  const [customerAddress, setAddress] = useState("");
-  const [customerContact, setContact] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  // const [name, setName] = useState("");
+  // const [customerAddress, setAddress] = useState("");
+  // const [customerContact, setContact] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [error, setError] = useState(false);
 
   
 
   const handled = async (e) => {
     e.preventDefault();
-    setError(false)
-    try{
-      const res = await axios.post("http://localhost:1234/customer/add/dto", {
-        name,
-        customerAddress,
-        customerContact,
-        email,
-        password
-      })
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
 
-      const cart = await axios.post("http://localhost:1234/cart/add/dto", {
-      })
+    // setError(false)
+    // try{
+    //   const res = await axios.post("http://localhost:1234/customer/add/dto", {
+    //     name,
+    //     customerAddress,
+    //     customerContact,
+    //     email,
+    //     password
+    //   })
 
-      cart.data && window.location.replace("/login");
-    }catch(err){
-      setError(true);
-    }  
+    //   const cart = await axios.post("http://localhost:1234/cart/add/dto", {
+    //   })
+
+    //   cart.data && window.location.replace("/login");
+    // }catch(err){
+    //   setError(true);
+    // }  
   }
+
+  const initalValues = { name: "" , address:"", contact:"", email:"", password: ""};
+  const [formValues, setFormValues] = useState(initalValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]:value});
+    console.log(formValues);
+  }
+
+  const validate = (values) => {
+    const errors = {}
+    const regex =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regexContact = /^\s*-?[0-9]{1,10}\s*$/;
+    const regexTest = /^[a-zA-Z ]*$/;
+
+    if(!values.name) {
+      errors.name = "Name is required";
+    }  else if(values.name.length < 4 ) {
+      errors.name = "Name must be more than 3 characters";
+    } else if(values.name.length > 15 ) {
+      errors.name = "Name cannot exceed more than 15 characters";
+    } else if(!regexTest.test(values.name)) {
+      errors.name = "Only text characters allowed";
+    }
+
+    if(!values.address) {
+      errors.address = "City is required";
+    } else if(values.address.length < 4 ) {
+      errors.address = "City must be more than 3 characters";
+    } else if(values.address.length > 10 ) {
+      errors.address = "City cannot exceed more than 10 characters";
+    }
+
+    if(!values.contact) {
+      errors.contact = "Contact is required";
+    } else if(!regexContact.test(values.contact)) {
+      errors.contact = "Enter a valid number";
+    }
+    
+    if(!values.email) {
+      errors.email = "Email is required";
+    } else if(!regex.test(values.email)) {
+      errors.email = "This is not a valid email format";
+    }
+    
+    if(!values.password) {
+      errors.password = "Password is required";
+    } else if(values.password.length < 4 ) {
+      errors.password = "Password must be more than 4 characters";
+    } else if(values.password.length > 10 ) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    return errors;
+  }
+
+
   return (
     <div className='section reg'>  
       <div className='regImg'>
@@ -43,18 +105,23 @@ const Customer = () => {
       <h1 className='register'>Register as <span> Customer</span></h1>
         <div className='inputFields'>
           <label htmlFor="name">Name</label>
-          <input type="text" onChange={(e) => setName(e.target.value)}/>
-          <label htmlFor="address">Address</label>
-          <input type="text" onChange={(e) => setAddress(e.target.value)}/>
+          <input type="text" value={formValues.name} onChange={handleChange} name='name'/>
+          <span>{formErrors.name}</span>
+          <label htmlFor="address">City</label>
+          <input type="text" value={formValues.address} onChange={handleChange} name='address'/>
+          <span>{formErrors.address}</span>
           <label htmlFor="contact">Contact</label>
-          <input type="number" onChange={(e) => setContact(e.target.value)}/>
+          <input type="number" value={formValues.contact} onChange={handleChange} name='contact'/>
+          <span>{formErrors.contact}</span>
           <label htmlFor="email">Email ID</label>
-          <input type="email" onChange={(e) => setEmail(e.target.value)}/>
+          <input type="email" value={formValues.email} onChange={handleChange} name='email'/>
+          <span>{formErrors.email}</span>
           <label htmlFor="password">Password</label>
-          <input type="password" onChange={(e) => setPassword(e.target.value)}/>
+          <input type="password" value={formValues.password} onChange={handleChange} name='password'/>
+          <span>{formErrors.password}</span>
         </div>
           <input value='Register'  type='submit' />
-        { error && console.log(error) }
+        {/* { error && console.log(error) } */}
       </form>
     </div>
   )
